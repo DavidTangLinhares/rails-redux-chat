e14421b33e9457e1a9e8ecba6d5e5b2687de69dd
David Tang <david_tang@web.de>
2025-10-29 12:48:42 +0100

    Minimal Show page fetching messages

---
+++ app/javascript/chat/index.jsx
@@ -0,0 +1,44 @@
+// app/javascript/chat/index.jsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import ReduxPromise from "redux-promise";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./components/app";
+import messagesReducer from "./reducers/messages_reducer";
+
+// Grab the Rails div
+const chatContainer = document.getElementById("chat_app");
+
+// Initial Redux state
+const initialState = {
+  messages: [],
+  channels: ["general", "react", "paris"], // TODO: fetch from Rails DB
+};
+
+// Combine reducers
+const reducers = combineReducers({
+  messages: messagesReducer,
+  channels: (state = null, action) => state,
+});
+
+// Apply middlewares and create store
+const middlewares = applyMiddleware(logger, ReduxPromise);
+const store = createStore(reducers, initialState, middlewares);
+
+// Create React 18 root
+const root = createRoot(chatContainer);
+
+// Render React app
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/channels/:channel" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
+  mode: "production", // or "development" during dev
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
+    chat: "./app/javascript/chat.js"
   },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    path: path.resolve(__dirname, "app/assets/builds")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,        // matches .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"] // allows importing without extensions
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
   ]
-}
+};
